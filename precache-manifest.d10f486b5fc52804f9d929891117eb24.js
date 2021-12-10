297ba782f4f824143fc14c41aa9fc421da83ca14
Ognjen Radočaj <ognjen.radocaj@nova.hr>
2021-12-10 10:59:55 +0100

    Updates

---
+++ precache-manifest.d10f486b5fc52804f9d929891117eb24.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7152b16657ea36c42874d81384906f1",
+    "url": "/youtube-clone/index.html"
+  },
+  {
+    "revision": "e35db613a5d600fe239a",
+    "url": "/youtube-clone/static/css/main.f966bcd5.chunk.css"
+  },
+  {
+    "revision": "9523e7bb16af50faa05a",
+    "url": "/youtube-clone/static/js/2.9aee8165.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/youtube-clone/static/js/2.9aee8165.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1bb81deb8afac661ea34",
+    "url": "/youtube-clone/static/js/3.c60a92fe.chunk.js"
+  },
+  {
+    "revision": "e35db613a5d600fe239a",
+    "url": "/youtube-clone/static/js/main.8d479edd.chunk.js"
+  },
+  {
+    "revision": "cdf733a509e72be5bfe3",
+    "url": "/youtube-clone/static/js/runtime-main.56474edd.js"
+  }
+]);
