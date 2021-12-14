585bc0e56b5fe2e13377f0864ccb6d94dcfe7ed0
oradocaj <oradocaj@gmail.com>
2021-12-14 22:35:28 +0100

    Finished Recommended Videos Component

--- src/RecommendedVideos.js
+++ src/RecommendedVideos.js
@@ -1,10 +1,77 @@
 import React from 'react';
 import './RecommendedVideos.css';
+import VideoCard from './VideoCard';
 
 function RecommendedVideos() {
     return (
         <div className='RecommendedVideos'>
-            <h2>This is where videos go!</h2>
+            <h2>Recommended</h2>
+            <div className='recommendedVideos__videos'>
+                <VideoCard
+                    title='Become a Web Developer in 10 minutes | 2019/2020'
+                    views='2.3M Views'
+                    timestamp='3 days ago'
+                    channelImage='https://avatars.githubusercontent.com/u/81166740?s=400&u=374c92a97cc84ddbee9d210fed03d4aae069865f&v=4'
+                    channel='Ogic Rogic'
+                    image='https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwallup.net%2Fwp-content%2Fuploads%2F2018%2F03%2F17%2F537815-web_development-desk-Flatdesign.jpg&f=1&nofb=1'
+                />
+                <VideoCard
+                    title='Become a Web Developer in 10 minutes | 2019/2020'
+                    views='2.3M Views'
+                    timestamp='3 days ago'
+                    channelImage='https://avatars.githubusercontent.com/u/81166740?s=400&u=374c92a97cc84ddbee9d210fed03d4aae069865f&v=4'
+                    channel='Ogic Rogic'
+                    image='https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwallup.net%2Fwp-content%2Fuploads%2F2018%2F03%2F17%2F537815-web_development-desk-Flatdesign.jpg&f=1&nofb=1'
+                />
+                <VideoCard
+                    title='Become a Web Developer in 10 minutes | 2019/2020'
+                    views='2.3M Views'
+                    timestamp='3 days ago'
+                    channelImage='https://avatars.githubusercontent.com/u/81166740?s=400&u=374c92a97cc84ddbee9d210fed03d4aae069865f&v=4'
+                    channel='Ogic Rogic'
+                    image='https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwallup.net%2Fwp-content%2Fuploads%2F2018%2F03%2F17%2F537815-web_development-desk-Flatdesign.jpg&f=1&nofb=1'
+                />
+                <VideoCard
+                    title='Become a Web Developer in 10 minutes | 2019/2020'
+                    views='2.3M Views'
+                    timestamp='3 days ago'
+                    channelImage='https://avatars.githubusercontent.com/u/81166740?s=400&u=374c92a97cc84ddbee9d210fed03d4aae069865f&v=4'
+                    channel='Ogic Rogic'
+                    image='https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwallup.net%2Fwp-content%2Fuploads%2F2018%2F03%2F17%2F537815-web_development-desk-Flatdesign.jpg&f=1&nofb=1'
+                />
+                <VideoCard
+                    title='Become a Web Developer in 10 minutes | 2019/2020'
+                    views='2.3M Views'
+                    timestamp='3 days ago'
+                    channelImage='https://avatars.githubusercontent.com/u/81166740?s=400&u=374c92a97cc84ddbee9d210fed03d4aae069865f&v=4'
+                    channel='Ogic Rogic'
+                    image='https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwallup.net%2Fwp-content%2Fuploads%2F2018%2F03%2F17%2F537815-web_development-desk-Flatdesign.jpg&f=1&nofb=1'
+                />
+                <VideoCard
+                    title='Become a Web Developer in 10 minutes | 2019/2020'
+                    views='2.3M Views'
+                    timestamp='3 days ago'
+                    channelImage='https://avatars.githubusercontent.com/u/81166740?s=400&u=374c92a97cc84ddbee9d210fed03d4aae069865f&v=4'
+                    channel='Ogic Rogic'
+                    image='https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwallup.net%2Fwp-content%2Fuploads%2F2018%2F03%2F17%2F537815-web_development-desk-Flatdesign.jpg&f=1&nofb=1'
+                />
+                <VideoCard
+                    title='Become a Web Developer in 10 minutes | 2019/2020'
+                    views='2.3M Views'
+                    timestamp='3 days ago'
+                    channelImage='https://avatars.githubusercontent.com/u/81166740?s=400&u=374c92a97cc84ddbee9d210fed03d4aae069865f&v=4'
+                    channel='Ogic Rogic'
+                    image='https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwallup.net%2Fwp-content%2Fuploads%2F2018%2F03%2F17%2F537815-web_development-desk-Flatdesign.jpg&f=1&nofb=1'
+                />
+                <VideoCard
+                    title='Become a Web Developer in 10 minutes | 2019/2020'
+                    views='2.3M Views'
+                    timestamp='3 days ago'
+                    channelImage='https://avatars.githubusercontent.com/u/81166740?s=400&u=374c92a97cc84ddbee9d210fed03d4aae069865f&v=4'
+                    channel='Ogic Rogic'
+                    image='https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwallup.net%2Fwp-content%2Fuploads%2F2018%2F03%2F17%2F537815-web_development-desk-Flatdesign.jpg&f=1&nofb=1'
+                />
+            </div>
         </div>
     )
 };
