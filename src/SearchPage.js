20802f498db74b38be8448427c04aa0fd6b18dc9
oradocaj <oradocaj@gmail.com>
2021-12-16 22:11:08 +0100

    Added ChannelRow component

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import  ChannelRow from './ChannelRow';
 import TuneOutlinedIcon from "@mui/icons-material/TuneOutlined"
 import './SearchPage.css';
 
@@ -7,7 +8,18 @@ function SearchPage() {
         <div className='searchPage'>
             <div className='searchPage__filter'>
                 <TuneOutlinedIcon/>
+                <h2>FILTER</h2>
             </div>
+            <hr/>
+
+            <ChannelRow
+                image='https://cdn.pixabay.com/photo/2015/05/31/13/29/lipstick-791761_960_720.jpg'
+                channel='Tanjica Bananica'
+                verified
+                subs='659K'
+                noOfVideos={382}
+                description='You can find some awesome makeup tutorials here.'        
+            />
         </div>
     )
 };
